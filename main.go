03f9a411619cd3711839cbf1901f9bfb43fc705c
4l3-j4ndr0 <al3jandro9400@gmail.com>
2023-10-07 01:51:31 -0400

    declaracion SWITCH

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"runtime"
 	"time"
 )
 
@@ -16,5 +17,49 @@ func main() {
 	} else {
 		fmt.Println("Noche")
 	}
-	
+
+
+	// ########  declaracion switch  ########
+
+	// especificando la variable con la que se va a trabajar
+	switch os := runtime.GOOS; os {
+	case "windows":
+		fmt.Println("Go run -> Windows")
+	case "linux":
+		fmt.Println("Go run -> Linux")
+	case "darwin":
+		fmt.Println("Go run -> macOS")
+	default:
+		fmt.Println("Go run -> Otro OS")
+	}
+
+	// sin especificar la variable con la que se va a trabajar y simplemente
+	// trabjando directamente con la variable
+	switch os := runtime.GOOS; {
+	case os == "windows":
+		fmt.Println("Go run -> Windows")
+	case os == "linux":
+		fmt.Println("Go run -> Linux")
+	case os == "darwin":
+		fmt.Println("Go run -> macOS")
+	default:
+		fmt.Println("Go run -> Otro OS")
+	}
+
+	// manera tradicional como en otros lenguajes
+	os := runtime.GOOS
+	switch os {
+	case "windows":
+		fmt.Println("Go run -> Windows")
+	case "linux":
+		fmt.Println("Go run -> Linux")
+	case "darwin":
+		fmt.Println("Go run -> macOS")
+	default:
+		fmt.Println("Go run -> Otro OS")
+	}
+
+	// Nota : No esta mal colocar el break al fial de cada caso pero no
+	// es necesario
+
 }
